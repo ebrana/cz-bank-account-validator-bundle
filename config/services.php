e543bb969379d44d1408d3e055783a0653cdcb8d
jakub-hurda <hurda@ebrana.cz>
2025-11-14 14:34:08 +0100

    [D020105][FIX] add service into container

--- config/services.php
+++ config/services.php
@@ -6,17 +6,24 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
 use Ebrana\CzBankAccountValidatorBundle\Provider\BankCodesProvider;
 use Ebrana\CzBankAccountValidatorBundle\Service\BankAccountNumberValidator;
+use Ebrana\CzBankAccountValidatorBundle\Validator\AccountNumberValidator;
 
 return static function (ContainerConfigurator $container) {
     $container->services()
         ->set('ebrana.bank_codes_provider', BankCodesProvider::class)
         ->alias(BankCodesProvider::class, 'ebrana.bank_codes_provider')
 
-        ->set('ebrana.cz_bank_validator', BankAccountNumberValidator::class)
+        ->set('ebrana.bank_account_number_validator', BankAccountNumberValidator::class)
             ->args([
                 service('ebrana.bank_codes_provider'),
             ])
-            ->tag('validator.constraint_validator')
-        ->alias(BankAccountNumberValidator::class, 'ebrana.cz_bank_validator')
+        ->alias(BankAccountNumberValidator::class, 'ebrana.bank_account_number_validator')
+
+        ->set('ebrana.account_number_validator', AccountNumberValidator::class)
+        ->args([
+            service('ebrana.bank_account_number_validator'),
+        ])
+        ->tag('validator.constraint_validator')
+        ->alias(AccountNumberValidator::class, 'ebrana.account_number_validator')
     ;
 };
