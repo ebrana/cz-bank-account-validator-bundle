8de985de0e8c60a569b2d63063b632736adbdea8
jakub-hurda <hurda@ebrana.cz>
2025-11-14 14:05:42 +0100

    [D020105][FIX] fix service definition

--- config/services.php
+++ config/services.php
@@ -10,7 +10,7 @@ use Ebrana\CzBankAccountValidatorBundle\Service\BankAccountNumberValidator;
 return static function (ContainerConfigurator $container) {
     $container->services()
         ->set('ebrana.bank_codes_provider', BankCodesProvider::class)
-        ->alias(BankCodesProvider::class, 'ebrana.cz_bank_validator')
+        ->alias(BankCodesProvider::class, 'ebrana.bank_codes_provider')
 
         ->set('ebrana.cz_bank_validator', BankAccountNumberValidator::class)
             ->args([
